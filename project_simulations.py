b5b009de23d0e5f27b76399af885eb8b6f0acfa2
Ben Rosenthal <50952285+brosenthal3@users.noreply.github.com>
2025-12-11 12:12:03 +0100

    optimize runtime of simulation

--- project_simulations.py
+++ project_simulations.py
@@ -1,4 +1,5 @@
 import numpy as np
+import time
 import matplotlib.pyplot as plt
 
 class PhaseField2DModel:
@@ -9,14 +10,14 @@ class PhaseField2DModel:
         self.types = [2, 2, 1, 2] # cell types
 
         # model parameters
-        self.kappa = 15 # interface energy coefficient
-        self.epsilon2 = 0.4 # interface thickness parameter
+        self.kappa = 8 # interface energy coefficient
+        self.epsilon2 = 0.8 # interface thickness parameter
         self.alpha = 0.01 # volume conservation parameter
-        self.mobility = 1.5 # mobility coefficient
+        self.mobility = 2 # mobility coefficient
         self.tau = 1 # relaxation time
-        self.gamma = 8 # no-overlap coefficient
-        self.adhesion = np.array([[0.01, 0.001],
-                                 [0.001, 0.6]]) # adhesion matrix between cell types
+        self.gamma = 22 # no-overlap coefficient
+        self.adhesion = np.array([[0.001, 0.001],
+                                 [0.001, 1.5]]) # adhesion matrix between cell types
 
         # Lists to shift rows and columns by one in the 4 directions
         self.sright = [(i+1)%self.Lx for i in range(self.Lx)] 
@@ -35,7 +36,7 @@ class PhaseField2DModel:
         for k in range(self.N):
             for i in range(self.Lx):
                 for j in range(self.Ly):
-                    if (i-(self.Lx/2 + spacing))**2 + (j-self.Ly/2)**2 < 9**2:
+                    if (i-(self.Lx/2 + spacing))**2 + (j-self.Ly/2)**2 < 13**2:
                         phi[i, j, k] = 1
 
             spacing += d_spacing
@@ -43,15 +44,31 @@ class PhaseField2DModel:
         return phi
 
 
-    def h(self, x):
-        return(x*x*(3-2*x))
+    def h(self, x, out=None):
+        """Smooth step polynomial h(x) with optional output buffer to reduce allocations."""
+        if out is None:
+            return x * x * (3 - 2 * x)
+        # In-place: out = x*x; then out *= (3 - 2*x)
+        np.multiply(x, x, out=out)
+        np.multiply(out, (3 - 2 * x), out=out)
+        return out
     
 
     def nabla_squared(self, phi):
-        return (phi[self.sright,:] + phi[self.sleft,:] + phi[:,self.sup] + phi[:,self.sdown] - 4*phi)
+        """Laplacian via roll; uses wraparound boundaries and avoids indexing lists."""
+        return (
+            np.roll(phi, 1, axis=0)
+            + np.roll(phi, -1, axis=0)
+            + np.roll(phi, 1, axis=1)
+            + np.roll(phi, -1, axis=1)
+            - 4 * phi
+        )
 
 
-    def run_simulation(self, tmax=250, dt=0.05):
+    def run_simulation(self, tmax=250, dt=0.025):
+        # time simulation
+        init_time = time.time()
+
         # Make Plot - use imshow for much faster rendering
         fig, ax = plt.subplots()
         plot_field = np.sum(self.phi, axis=2)
@@ -61,10 +78,11 @@ class PhaseField2DModel:
         plt.show()
 
         # compute volume at t=0
-        VolT = [np.sum(self.h(self.phi[:, :, k])) + 200 for k in range(self.N)]
-
+        VolT = [np.sum(self.h(self.phi[:, :, k])) for k in range(self.N)]
+        
         # Update of the matrix phi
         t=0
+        considered_combinations = []
         while t<tmax:
             for k in range(self.N):
                 Vol = np.sum(self.h(self.phi[:, :, k]))
@@ -76,11 +94,12 @@ class PhaseField2DModel:
                 # loop over all cells to calculate adhesion/repulsion terms
                 adhesion_repulsion_term = 0
                 for l in range(self.N):
-                    if l != k:
+                    if l != k and {k,l} not in considered_combinations:
                         k_cell_type = self.types[k]-1
                         l_cell_type = self.types[l]-1
                         adhesion_repulsion_term += interface * ( (6*self.gamma)/self.kappa) * self.h(self.phi[:,:,l]) - (6*self.adhesion[k_cell_type, l_cell_type]/self.kappa * self.nabla_squared(self.h(self.phi[:,:,l])))
-                
+                        considered_combinations.append({k,l})
+    
                 # calculate volume exclusion term
                 volume_exclusion_term = 6 * self.mobility * self.alpha * interface * (VolT[k]-Vol)
 
@@ -101,6 +120,9 @@ class PhaseField2DModel:
                 plt.pause(0.001)  # Small pause for display update
 
         print("Simulation complete.")
+        finish_time = time.time()
+        print(f"Total simulation time: {round(finish_time - init_time, 2)} seconds")
+
         plt.ioff()  # Turn off interactive mode
         plt.show()
 
